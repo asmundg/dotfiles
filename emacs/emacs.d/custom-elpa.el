04a6d7c24937cdc733bc9b52b56d31adce479315
Åsmund Grammeltvedt <asmundg@snap.tv>
2014-10-23 21:51:43 +0200

    Oops, defvar doesn't work that way

--- emacs/emacs.d/custom-elpa.el
+++ emacs/emacs.d/custom-elpa.el
@@ -39,7 +39,7 @@
 (require 'helm-config)
 (helm-mode 1)
 
-(defvar
+(setq
   helm-quick-update                     t ; do not display invisible candidates
   helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
   helm-buffers-fuzzy-matching           t ; fuzzy matching buffer names when non--nil
