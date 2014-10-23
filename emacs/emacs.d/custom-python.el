96206b735f21025faf514f72ba61e9b7a385e6de
Åsmund Grammeltvedt <asmundg@snap.tv>
2014-10-23 21:56:36 +0200

    Tidy up

--- emacs/emacs.d/custom-python.el
+++ emacs/emacs.d/custom-python.el
@@ -7,9 +7,11 @@
  '(flymake-warnline ((((class color)) (:underline "yellow")))))
 
 (defun project-directory (buffer-name)
-  "Returns the root directory of the project that contains the
-given buffer. Any directory with a .git or .jedi file/directory
-is considered to be a project root."
+  "Return root directory of the project containing BUFFER-NAME.
+
+Any directory with a .git or .jedi file/directory is considered
+to be a project root."
+
   (interactive)
   (let ((root-dir (file-name-directory buffer-name)))
     (while (and root-dir
@@ -22,7 +24,8 @@ is considered to be a project root."
     root-dir))
 
 (defun project-name (buffer-name)
-  "Returns the name of the project that contains the given buffer."
+  "Return name of the project containing BUFFER-NAME."
+
   (let ((root-dir (project-directory buffer-name)))
     (if root-dir
         (file-name-nondirectory
@@ -31,18 +34,32 @@ is considered to be a project root."
 
 (defun jedi-setup-venv ()
   "Activates the virtualenv of the current buffer."
+
   (require 'virtualenvwrapper)
   (let ((project-name (project-name buffer-file-name)))
     (when project-name (venv-workon project-name))))
 
-(venv-initialize-interactive-shells)
-(venv-initialize-eshell)
-(setq venv-location (expand-file-name "~/.virtualenvs"))
-(setq gud-pdb-command-name "python -m pdb")
+(defun setup-venv ()
+  "Initialize venv."
 
+  (require 'virtualenvwrapper)
+  (venv-initialize-interactive-shells)
+  (venv-initialize-eshell)
+  (defvar venv-location (expand-file-name "~/.virtualenvs"))
+  (defvar gud-pdb-command-name "python -m pdb"))
+
+(defun setup-py2cov ()
+  "Initialize py2cov coverage tool."
+
+  (require 'pycov2)
+  (pycov2-mode))
+
+
+(add-hook 'python-mode-hook 'setup-venv)
+;(add-hook 'python-mode-hook 'setup-py2cov)
 (add-hook 'python-mode-hook 'jedi:setup)
 (add-hook 'python-mode-hook 'jedi-setup-venv)
-(setq jedi:complete-on-dot t)
+(defvar jedi:complete-on-dot t)
 
 (provide 'custom-python)
 ;;; custom-python.el ends here
