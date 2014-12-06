2d3ed3de00a3de78f1c60becf217a26c89c0b925
Åsmund Grammeltvedt <asmundg@snap.tv>
2014-12-06 12:02:11 +0100

    Remove even more deprecated config

--- emacs/emacs.d/custom-js.el
+++ emacs/emacs.d/custom-js.el
@@ -1,27 +1,10 @@
 ;;; custom-js.el -- Javascript config
 ;;; Commentary:
 
-;; (when (load "flymake" t)
-;;   (defun flymake-gjslint-init ()
-;;     (let* ((temp-file (flymake-init-create-temp-buffer-copy
-;;                        'flymake-create-temp-inplace)))
-;;       (list "gjslint" (list temp-file "--nosummary --jslint_error all --strict"))))
-
-;;   (add-to-list 'flymake-allowed-file-name-masks
-;;                '(".+\\.js$"
-;;                  flymake-gjslint-init
-;;                  flymake-simple-cleanup
-;;                  flymake-get-real-file-name))
-
-;;   (add-to-list 'flymake-err-line-patterns
-;;                '("^Line \\([[:digit:]]+\\), E:[[:digit:]]+: "
-;;                  nil 1 nil)))
-
 ;;; Code:
 (require 'nvm)
 
 (add-to-list 'auto-mode-alist '("\\.js$" . javascript-mode))
-;;(add-hook 'javascript-mode-hook 'flymake-mode-on)
 
 (defun load-nvm ()
   "Initialize nvm."
