ce3f9d0eba9276750606c93cdcc48ff701822373
Åsmund Grammeltvedt <asmundg@snap.tv>
2015-10-21 16:23:23 +0200

    Bump magit

--- emacs/emacs.d/config/custom-git.el
+++ emacs/emacs.d/config/custom-git.el
@@ -1,11 +1,12 @@
+;;; custom-git.el -- Git config
+;;; Commentary:
+
+;;; Code:
+
 (global-set-key (kbd "C-x v s") 'magit-status)
 ;; change magit diff colors
 (eval-after-load 'magit
   '(progn
-     (set-face-foreground 'magit-diff-add "green3")
-     (set-face-background 'magit-diff-add "black")
-     (set-face-foreground 'magit-diff-del "red3")
-     (set-face-background 'magit-diff-del "black")
      (defun magit-highlight-section ())
      (when (not window-system)
        (set-face-background 'magit-item-highlight "black"))))
@@ -15,4 +16,7 @@
 (autoload 'mo-git-blame-current "mo-git-blame" nil t)
 (global-set-key (kbd "C-x v b") 'mo-git-blame-current)
 
+(setq magit-last-seen-setup-instructions "1.4.0")
+
 (provide 'custom-git)
+;;; custom-git.el ends here
