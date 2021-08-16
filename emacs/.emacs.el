083b3c679e66e48569f0b1869c6333a53cd9246f
Åsmund Grammeltvedt <asgramme@microsoft.com>
2021-08-16 14:23:38 +0200

    Stop fish from screwing up PATH by emitting warnings

--- emacs/.emacs.el
+++ emacs/.emacs.el
@@ -88,6 +88,7 @@ With argument ARG, do this that many times."
   :bind (("C-M-=" . default-text-scale-increase)
          ("C-M--" . default-text-scale-decrease)))
 
+(setenv "TERM" "screen-256color")
 (let ((path-from-shell (shell-command-to-string "/usr/local/bin/fish -i -c \"echo -n \\$PATH[1]; for val in \\$PATH[2..-1];echo -n \\\":\\$val\\\";end\"")))
   (setenv "PATH" path-from-shell)
   (setq exec-path (split-string path-from-shell ":")))
